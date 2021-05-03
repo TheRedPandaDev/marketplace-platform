b49b46c0f638c5d74b6cbb4ba52d4a9b5aa169ee
RealThomasMiles <Dmilya2000@gmail.com>
2021-05-03 12:30:30 +0300

    Added responses

---
+++ src/main/java/com/realthomasmiles/marketplace/dto/response/ResponseError.java
@@ -0,0 +1,20 @@
+package com.realthomasmiles.marketplace.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResponseError {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
