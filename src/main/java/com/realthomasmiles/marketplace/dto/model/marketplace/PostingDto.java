2b9897aee43cb2c4abd1f3a5b3561186a471c872
RealThomasMiles <Dmilya2000@gmail.com>
2021-05-03 12:28:28 +0300

    Added Posting DTO

---
+++ src/main/java/com/realthomasmiles/marketplace/dto/model/marketplace/PostingDto.java
@@ -0,0 +1,26 @@
+package com.realthomasmiles.marketplace.dto.model.marketplace;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PostingDto {
+
+    private String article;
+    private Boolean isActive;
+    private Long categoryId;
+    private Long authorId;
+    private Long locationId;
+    private String posted;
+    private String name;
+    private String description;
+    private Long price;
+
+}
