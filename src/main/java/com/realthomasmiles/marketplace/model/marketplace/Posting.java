a525279539a643d7aa0f384d0fcf3d9b21355506
RealThomasMiles <Dmilya2000@gmail.com>
2021-05-03 12:28:21 +0300

    Added Posting model

---
+++ src/main/java/com/realthomasmiles/marketplace/model/marketplace/Posting.java
@@ -0,0 +1,34 @@
+package com.realthomasmiles.marketplace.model.marketplace;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
+@Table(name = "postings")
+public class Posting {
+
+    @Id
+    private Long id;
+    private String article;
+    @Column(name = "is_active")
+    private Boolean isActive;
+    @Column(name = "category_id")
+    private Long categoryId;
+    @Column(name = "author_id")
+    private Long authorId;
+    @Column(name = "location_id")
+    private Long locationId;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date posted;
+    private String name;
+    private String description;
+    private Long price;
+
+}
