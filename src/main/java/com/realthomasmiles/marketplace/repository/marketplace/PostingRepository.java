bd239c75db7ad1a10f3777801236f76812b6a725
RealThomasMiles <Dmilya2000@gmail.com>
2021-05-03 12:29:00 +0300

    Added Posting repository

---
+++ src/main/java/com/realthomasmiles/marketplace/repository/marketplace/PostingRepository.java
@@ -0,0 +1,8 @@
+package com.realthomasmiles.marketplace.repository.marketplace;
+
+import com.realthomasmiles.marketplace.model.marketplace.Posting;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostingRepository extends CrudRepository<Posting, Long> {
+
+}
