c8112645d9b776ee547d3100e820ecb6950e3226
RealThomasMiles <Dmilya2000@gmail.com>
2021-05-08 18:13:22 +0300

    Added BCryptPasswordEncoder bean

--- src/main/java/com/realthomasmiles/marketplace/config/MarketplaceConfiguration.java
+++ src/main/java/com/realthomasmiles/marketplace/config/MarketplaceConfiguration.java
@@ -4,9 +4,15 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.NamingConventions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class MarketplaceConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public ModelMapper modelMapper() {
