68b4489844ac517ef7dd526350b0f4e76057565f
RealThomasMiles <Dmilya2000@gmail.com>
2021-05-14 13:33:20 +0300

    Added file upload folder expose

--- src/main/java/com/realthomasmiles/marketplace/config/PageConfig.java
+++ src/main/java/com/realthomasmiles/marketplace/config/PageConfig.java
@@ -1,8 +1,12 @@
 package com.realthomasmiles.marketplace.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class PageConfig implements WebMvcConfigurer {
@@ -17,4 +21,17 @@ public class PageConfig implements WebMvcConfigurer {
         registry.addViewController("/myProfile").setViewName("profile");
     }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        exposeDirectory("postings-photos", registry);
+    }
+
+    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+        Path uploadDir = Paths.get(dirName);
+        String uploadPath = uploadDir.toFile().getAbsolutePath();
+
+        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+
+        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+    }
 }
